5cc9d21e6c9d0d3dc3ea90d80344ed07418fefb3
johnliter <te07civicsi@gmail.com>
2026-01-18 20:53:34 -0600

    Update .gitignore to include inc/db.php and add config.php for database configuration

---
+++ inc/config.php
@@ -0,0 +1,6 @@
+$env = parse_ini_file(__DIR__ . '/../.env');
+
+define('DB_HOST', $env['DB_HOST']);
+define('DB_NAME', $env['DB_NAME']);
+define('DB_USER', $env['DB_USER']);
+define('DB_PASS', $env['DB_PASS']);
